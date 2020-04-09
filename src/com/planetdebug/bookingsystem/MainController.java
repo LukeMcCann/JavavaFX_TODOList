deddc8eaf04894edf6ec157285c224ba6dbc027e
Luke McCann <developer-lukemccann@outlook.com>
2020-04-09 19:01:14 +0100

    Adds FXML annotation to EventHandler onButtonClicked

--- src/com/planetdebug/bookingsystem/MainController.java
+++ src/com/planetdebug/bookingsystem/MainController.java
@@ -5,9 +5,12 @@ import javafx.scene.control.TextField;
 
 public class MainController
 {
+    // FXML ids are mapped to variables by the @FXML annotation.
     @FXML
     private TextField nameField;
 
+    // It is best practice to annotate EventHandlers for other developers to easily derive which methods are handlers.
+    @FXML
     public void onButtonClicked()
     {
         System.out.println("Hello, " + this.nameField.getText());
