63c31eb1eddfafa9b27e7a02902203fc6e8d6146
Luke McCann <developer-lukemccann@outlook.com>
2020-04-09 18:58:31 +0100

    Adds EventHandler example for passing information from GUI to controller

--- src/com/planetdebug/bookingsystem/MainController.java
+++ src/com/planetdebug/bookingsystem/MainController.java
@@ -1,9 +1,15 @@
 package com.planetdebug.bookingsystem;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class MainController
 {
+    @FXML
+    private TextField nameField;
+
     public void onButtonClicked()
     {
-        System.out.println("Hello World!");
+        System.out.println("Hello, " + this.nameField.getText());
     }
 }
